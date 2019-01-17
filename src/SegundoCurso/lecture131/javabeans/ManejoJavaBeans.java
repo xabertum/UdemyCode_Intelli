11aaa691ee99e9ee664383aa294be8a6ccd811a2
xabertum <mohatar@gmail.com>
2019-01-17 12:40:41 +0100

    Added example

--- src/SegundoCurso/lecture131/javabeans/ManejoJavaBeans.java
+++ src/SegundoCurso/lecture131/javabeans/ManejoJavaBeans.java
@@ -1,4 +1,14 @@
 package lecture131.javabeans;
 
 public class ManejoJavaBeans {
+
+    public static void main (String[] args) {
+        PersonaBean bean = new PersonaBean();
+        bean.setNombre("Juan");
+        bean.setEdad(25);
+
+        System.out.println("Nombre: " + bean.getNombre());
+        System.out.println("Edad: " + bean.getEdad());
+    }
+
 }
