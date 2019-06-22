16ce84b2c3e5c7b1a02c0b643ccff6f9234aebc4
xabertum <mohatar@gmail.com>
2019-06-22 10:55:52 +0200

    JDBC intro completed

--- src/TercerCurso_JDBC/lecture162/introduccionJDBC/IntroduccionJDBC.java
+++ src/TercerCurso_JDBC/lecture162/introduccionJDBC/IntroduccionJDBC.java
@@ -1,33 +1,40 @@
 package lecture162.introduccionJDBC;
-import javax.swing.plaf.nimbus.State;
+
 import java.sql.*;
 
 public class IntroduccionJDBC {
 
-  public static void main (String args []) {
-    String url ="jdbc:mysql://localhost:3306/jdbc_test?useSSL=false";
+  public static void main(String [] args) {
+    String url = "jdbc:mysql://localhost:3306/jdbc_test?useSSL=false&serverTimezone=UTC";
 
     // Cargamos el driver de MySQL
 
     try {
-      Class.forName("com.mysql.jdbc.Driver");
+      Class.forName("com.mysql.cj.jdbc.Driver");
       // Creamos el objeto conexión
-      Connection connection = (Connection) DriverManager.getConnection(url, "roor", "zanfona");
+      Connection connection = DriverManager.getConnection(url, "root", "zanfona");
 
       // Creamos el objeto Statement
       Statement statement = connection.createStatement();
 
       // Creamos el query
 
-      String sql = "Select * "
+      String sql = "SELECT id_persona, nombre, apellido FROM persona";
+      ResultSet resultSet = statement.executeQuery(sql);
 
+      while (resultSet.next()) {
+        System.out.println("Id: " + resultSet.getInt(1));
+        System.out.println(" Nombre: " + resultSet.getString(2));
+        System.out.println(" Apellido: " + resultSet.getString(3));
+      }
 
+      resultSet.close();
+      statement.close();
+      connection.close();
 
-    } catch (ClassNotFoundException ex) {
+    } catch (ClassNotFoundException | SQLException ex) {
       ex.printStackTrace();
 
-    } catch (SQLException e) {
-      e.printStackTrace();
     }
 
   }
