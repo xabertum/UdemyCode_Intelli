804461bf901b5200c5aff66a1b0e8b82e6f32187
xabertum <mohatar@gmail.com>
2019-07-30 15:05:46 +0200

    Added personaDAO Interface

--- src/TercerCurso_JDBC/lecture172/personasCapaDatos/personasJDBC/personaDAO.java
+++ src/TercerCurso_JDBC/lecture172/personasCapaDatos/personasJDBC/personaDAO.java
@@ -1,5 +1,14 @@
 package lecture172.personasCapaDatos.personasJDBC;
 
-public class personaDAO {
+import lecture172.personasCapaDatos.personasDTO.personasDTO;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface personaDAO {
+	
+	public int insert(personasDTO persona) throws SQLException;
+	public int update(personasDTO persona) throws SQLException;
+	public int delete(personasDTO persona) throws SQLException;
+	public List<personasDTO> select() throws SQLException;
 
 }
