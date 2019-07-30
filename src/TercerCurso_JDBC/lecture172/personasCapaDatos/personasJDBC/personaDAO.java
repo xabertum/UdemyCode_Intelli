312c9f528dc74a3ad7ff51d2b990bf16cba4c617
xabertum <mohatar@gmail.com>
2019-07-30 15:07:52 +0200

    Added coment

--- src/TercerCurso_JDBC/lecture172/personasCapaDatos/personasJDBC/personaDAO.java
+++ src/TercerCurso_JDBC/lecture172/personasCapaDatos/personasJDBC/personaDAO.java
@@ -4,6 +4,13 @@ import lecture172.personasCapaDatos.personasDTO.personasDTO;
 import java.sql.SQLException;
 import java.util.List;
 
+/**
+ * Esta interfaz contiene los metodos abstractos con las
+ * operaciones CRUD basicas sobre la tabla Persona.
+ * @author xabertum
+ *
+ */
+
 public interface personaDAO {
 	
 	public int insert(personasDTO persona) throws SQLException;
